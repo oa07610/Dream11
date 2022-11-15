387294b7c805d6481a67a459c1108397a0549abf
Owais Aijaz <owaisaijaz2003@gmail.com>
2022-11-15 12:11:13 +0500

    Ye le bhai

---
+++ settingsUserControl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dream11
+{
+    public partial class settingsUserControl : UserControl
+    {
+        public settingsUserControl()
+        {
+            InitializeComponent();
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "STOP";
+            }
+            else
+            {
+                checkBox1.Text = "PLAY";
+                Form1.sp.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "STOP";
+                Form1.sp.Play();
+            }
+            else
+            {
+                checkBox1.Text = "PLAY";
+                Form1.sp.Stop();
+            }
+        }
+    }
+}
